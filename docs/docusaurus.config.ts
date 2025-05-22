fc9c159eca5ebac75eb05d219b054d222902f02d
Karol Leśniak <16785518+kajkal@users.noreply.github.com>
2025-05-23 01:13:15 +0200

    Setup Docusaurus
    Create docs skeleton, add GEOS.js logo

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,91 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+
+const config: Config = {
+    title: 'GEOS.js',
+    tagline: 'an easy-to-use JavaScript wrapper over WebAssembly build of GEOS',
+    favicon: 'img/favicon.ico',
+
+    url: 'https://kajkal.github.io',
+    baseUrl: '/',
+
+    organizationName: 'kajkal',
+    projectName: 'geos.js',
+    deploymentBranch: 'gh-pages',
+    trailingSlash: false,
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    i18n: {
+        defaultLocale: 'en',
+        locales: [ 'en' ],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                },
+                blog: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        colorMode: {
+            respectPrefersColorScheme: true,
+        },
+        docs: {
+            sidebar: {
+                hideable: true,
+            },
+        },
+        navbar: {
+            title: 'GEOS.js',
+            logo: {
+                alt: 'GEOS.js Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Docs',
+                },
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'apiSidebar',
+                    position: 'left',
+                    label: 'API',
+                },
+                {
+                    href: 'https://github.com/kajkal/geos.js',
+                    position: 'right',
+                    className: 'header-github-link',
+                    title: 'GitHub repository',
+                    'aria-label': 'GitHub repository',
+                },
+            ],
+            hideOnScroll: true,
+        },
+        footer: {
+            style: 'dark',
+            copyright: `Copyright © ${new Date().getFullYear()} GEOS.js</br>Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.duotoneLight,
+            darkTheme: prismThemes.gruvboxMaterialDark,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className='container'>
+                <Heading as='h1' className='hero__title'>
+                    {siteConfig.title}
+                </Heading>
+                <p className='hero__subtitle'>
+                    {siteConfig.tagline}
+                </p>
+                <div className={styles.buttons}>
+                    <Link
+                        className='button button--secondary button--lg'
+                        to='/docs/quick-start'>
+                        Quick Start
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): ReactNode {
+    return (
+        <Layout
+            description='an easy-to-use JavaScript wrapper over WebAssembly build of GEOS'
+        >
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
+}
